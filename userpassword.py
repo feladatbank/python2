2fceb0c11eadf57cf7fadee849083d7e88593886
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-04-12 18:17:13 +0200

    Update userpassword.py

--- userpassword.py
+++ userpassword.py
@@ -3,25 +3,25 @@ Bejelentkezés
 
 Írjon programot, amely azt vizsgálja, hogy egy felhasználó helyesen adja-e meg a jelszavát! 
 A program addig kérdezi újra a felhasználónév-jelszó párost, amíg a felhasználó mindkettőt hibátlanul meg nem adja. 
-A program egyetlen felhasználó (bori99) jelszavát (Jelszo) ismeri, csak ezt a párost fogadja el helyesként. 
+A program egyetlen felhasználó (sis) jelszavát (1234) ismeri, csak ezt a párost fogadja el helyesként. 
 Mind a sikertelen, mind a sikeres bejelentkezési kísérletekről üzenetet ír a képernyőre.
 A program üzeneteinek megfogalmazásában kövesse az alábbi mintát:
 -----
-Adja meg a felhasználónevét! bori99
-Adja meg a jelszavát! Jelszo
+Adja meg a felhasználónevét! sis
+Adja meg a jelszavát! 1234
 Belépés engedélyezve.
 -----
 Adja meg a felhasználónevét! Bagaméri
 Adja meg a jelszavát! A kankalin sötétben virágzik!
 Belépés megtagadva.
-Adja meg a felhasználónevét! bori99
+Adja meg a felhasználónevét! bori69
 Adja meg a jelszavát! hibásjelszó
 Belépés megtagadva.
 Adja meg a felhasználónevét! hibásfelhasználó
-Adja meg a jelszavát! Jelszo
+Adja meg a jelszavát! Jelszooo
 Belépés megtagadva.
-Adja meg a felhasználónevét! bori99
-Adja meg a jelszavát! Jelszo
+Adja meg a felhasználónevét! sis
+Adja meg a jelszavát! 1234
 Belépés engedélyezve.
 '''
 
@@ -29,8 +29,8 @@ user = input("Kérem a felhasználó nevét!")
 password = input("Kérem a jelszót!")
 
 
-felhasznalo_nev = "bori99"
-jelszo = "Jelszo"
+felhasznalo_nev = "sis"
+jelszo = "1234"
 
 while True:
     if user == felhasznalo_nev and password == jelszo:
