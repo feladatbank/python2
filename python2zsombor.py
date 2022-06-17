37ae13a93e7a8d085d80d54d5fcd9b68aceefdc6
Balázs <71691881+hadaszibalazs205@users.noreply.github.com>
2022-06-17 17:15:05 +0200

    Update python2zsombor.py

--- python2zsombor.py
+++ python2zsombor.py
@@ -1,4 +1,6 @@
-# Csinálj egy függvényt / funkciót aminek meg lehet adni 5 számot, ezután vissza adja az 5 szám közül a legkissebb és a legnagyobb számot és az összegüket.
+'''
+Csinálj egy függvényt / funkciót aminek meg lehet adni 5 számot, ezután vissza adja az 5 szám közül a legkissebb és a legnagyobb számot és az összegüket.
+'''
 
 def szam(a, b, c, d, e):
     lista = [a, b, c, d, e]
@@ -7,6 +9,5 @@ def szam(a, b, c, d, e):
     print("A legkisebb szám:", lista[:1])
     print("A legnagyobb szám:", lista[4:])
     print("A számok összege: ", sum(lista))
-
 
 szam(1, 2, 4, 8, 16)
